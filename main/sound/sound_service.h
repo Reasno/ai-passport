ecdecac0ed91268368adce531e87963d1688c20f
guxi.reasno <guxi.reasno@bytedance.com>
2026-09-03 10:10:15 +0800

    feat(games): add buzzer countdown sounds
    Co-Authored-By: Aime <aime@bytedance.com>

--- main/sound/sound_service.h
+++ main/sound/sound_service.h
@@ -3,7 +3,14 @@
 #include "freertos/FreeRTOS.h"
 #include <stdbool.h>
 
-typedef enum { SOUND_TICK, SOUND_DING, SOUND_DU, SOUND_FANFARE, SOUND_FIND_RING } sound_effect_t;
+typedef enum {
+    SOUND_TICK,
+    SOUND_DING,
+    SOUND_DU,
+    SOUND_FANFARE,
+    SOUND_FIND_RING,
+    SOUND_BUZZER_GO,
+} sound_effect_t;
 esp_err_t sound_service_start(void);
 void sound_service_play(sound_effect_t effect);
 void sound_service_stop_ring(void);
